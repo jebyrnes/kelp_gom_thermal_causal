35dba7d29a734bb8db0c3fb720fe1921c73ff761
jebyrnes <jarrett.byrnes@umb.edu>
2021-05-27 23:10:12 -0400

    Add two panel plot

--- scripts/3_model_viz.R
+++ scripts/3_model_viz.R
@@ -18,3 +18,91 @@ setwd(here::here())
 
 DF.join <- read_csv("derived_data/combined_data_for_analysis.csv")
 mod_urchin_add <- readRDS("derived_data/mod_urchin_add.RDS")
+
+
+# Some info we will need
+region_df <- DF.join %>%
+    group_by(region) %>%
+    summarize(mean_temp_mn = mean_temp_mn[1],
+              urchin_mn = urchin_mn[1])
+
+timeseries_df <- DF.join %>%
+    group_by(year) %>%
+    summarize(urchin_dev = mean(urchin_dev, na.rm=TRUE),
+              mean_temp_dev = mean(mean_temp_dev, na.rm=TRUE),
+              urchin = mean(urchin, na.rm = TRUE)) %>%
+    arrange(year)
+
+
+# heatmap-a-palooza
+make_kelp_heatmap <- function(mean_temp_mn = 15, urchin_mn = 0,
+                              xlim = c(0,80)){
+    visreg::visreg2d(mod_urchin_add, xvar = "urchin_dev", yvar = "mean_temp_dev", 
+                     scale = "response", plot.type="gg", 
+                     cond = list(urchin_mn = urchin_mn,
+                                 mean_temp_mn = mean_temp_mn)) +
+        scale_fill_gradient2(low = "red", high = "blue", mid = "white", 
+                             midpoint = 0.5, limits = c(0,1)) +
+        xlim(xlim) +
+        labs(fill = "Percent Cover of Kelp",
+             y = "Deviation from Regional Temperature Mean",
+             x = "Deviation from Regional Urchin Mean",
+             subtitle = paste0("Assumes regional mean summer temp of ",
+                               mean_temp_mn, "C \nand urchin abundance of ", urchin_mn," per sq m"))
+    
+    
+}
+
+
+# two sites at 0 and 50 urchins
+# note - we start with known urchin density and temp
+# then subtract our regional means to get the anomalies
+
+pred_frame <- crossing(region_df,
+         urchin  = c(0,50),
+                temp = seq(11, 18, length.out = 100),
+         year = 2015) %>%
+    mutate(mean_temp_dev = temp - mean_temp_mn,
+           urchin_dev = urchin - urchin_mn)
+
+pred_values <- predict(mod_urchin_add, 
+                                newdata = pred_frame, 
+                                re.form = NULL, 
+                                type = "response",
+                                se.fit = TRUE)
+
+pred_frame <- pred_frame %>%
+    mutate(kelp.perc = pred_values$fit,
+           lwr = kelp.perc - 1*pred_values$se.fit,
+           upr = kelp.perc + 1*pred_values$se.fit)
+#all sites
+ggplot(pred_frame,
+       aes(x = temp,
+           y = kelp.perc,
+           group = region, 
+           color = mean_temp_mn)) +
+    geom_line() +
+    facet_wrap(~urchin, ncol = 1) +
+    scale_color_gradient(low = "blue", high = "red")
+# 
+
+#york v. downeast
+ggplot(pred_frame %>% 
+           filter(region %in% c("downeast", "york")) %>%
+           mutate(urchin = paste0(urchin, " urchins"),
+                  region = stringr::str_to_title(region)),
+       aes(x = temp,
+           y = kelp.perc*100,
+           group = region, 
+           color = region)) +
+    geom_line(size = 2) +
+    geom_ribbon(aes(ymin = lwr*100, ymax = upr*100), alpha = 0.2, color = NA, fill = "grey") +
+    facet_wrap(~urchin, ncol = 1) +
+    scale_color_manual(values = c("blue", "red")) +
+    theme_bw() +
+    labs(color = "",
+         x = "Temperature C",
+         y = "Percent Cover Kelp")
+
+
+
